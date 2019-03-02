1976cd631396b8b889baeeacaf9c91c6c939ca5b
c p <1107223894@qq.com>
2019-03-03 01:05:09 +0800

    修改coverImage

--- PlayerDemo/PlayerDemo/DemoViewController.swift
+++ PlayerDemo/PlayerDemo/DemoViewController.swift
@@ -24,55 +24,55 @@ class DemoViewController: UIViewController, UITableViewDataSource, UITableViewDe
         let assets = [
             (title: "bipbop basic master playlist",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop basic 400x300 @ 232 kbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear1/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop basic 640x480 @ 650 kbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear2/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop basic 640x480 @ 1 Mbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear3/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop basic 960x720 @ 2 Mbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear4/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop basic 22.050Hz stereo @ 40 kbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear0/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop advanced master playlist",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop advanced 416x234 @ 265 kbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/gear1/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop advanced 640x360 @ 580 kbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/gear2/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop advanced 960x540 @ 910 kbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/gear3/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop advanced 1280x720 @ 1 Mbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/gear4/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop advanced 1920x1080 @ 2 Mbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/gear5/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!),
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!),
             
             (title: "bipbop advanced 22.050Hz stereo @ 40 kbps",
              contentURL: URL(string: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/gear0/prog_index.m3u8")!,
-             coverURL: URL(string: "https://github.com/cp110/Player/blob/master/Screenshots/coverImage.png")!)
+             coverURL: URL(string: "https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240")!)
         ]
         return assets
     }()
